61906109ebf2c1cbd62e39dd616df188af008866
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2020-01-31 11:53:08 +0200

    Add test data and cheacking.

---
+++ scr/WebApplication1/Controllers/DrinkController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data.Interfacies;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly ICategoryRepository categoryRepository;
+        private readonly IDrinkRepository drinkRepository;
+
+        public DrinkController(ICategoryRepository repoCategory, IDrinkRepository repoDrink)
+        {
+            this.categoryRepository = repoCategory;
+            this.drinkRepository = repoDrink;
+        }
+
+        public ViewResult List(string category)
+        {
+            string _category = category;
+            IEnumerable<Drink> drinks;
+            string currentCategory = string.Empty;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                drinks = drinkRepository.Drinks.OrderBy(n => n.DrinkId);
+                currentCategory = "All drinks";
+            }
+            else
+            {
+                if (string.Equals("Alcoholic", _category, StringComparison.OrdinalIgnoreCase))
+                {
+                    drinks = drinkRepository.Drinks.Where(p => p.Category.CategoryName.Equals("Alcoholic")).OrderBy(p => p.Name);
+                }
+                else
+                {
+                    drinks = drinkRepository.Drinks.Where(p => p.Category.CategoryName.Equals("Non-Alcohol")).OrderBy(p => p.Name);
+
+                    currentCategory = _category;
+                }
+
+            }
+            var drinkListViewModel = new DrinkListViewModel
+            {
+                Drinks = drinks,
+                CurrentCategory = currentCategory
+            };
+            return View(drinkListViewModel);
+        }
+    }
+}
---
+++ scr/WebApplication1/Data/Repositories/Mocks/MockCategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data.Interfacies;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data.Repositories.Mocks
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Categories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                      new Category{CategoryName="Alcoholic", Description="All alcoholic drink"},
+                      new Category{CategoryName="Non-Alcohol",Description="All non-alcoholic drinks"}
+                };
+            }
+        }
+    }
+}
