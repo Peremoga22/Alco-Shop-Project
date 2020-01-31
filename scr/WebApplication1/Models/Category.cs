ba2f8fb210b30409ee387bce336a267a3306d9a9
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2020-01-31 11:35:00 +0200

    Add models

---
+++ scr/WebApplication1/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Drink> Drinks { get; set; }
+    }
+}
