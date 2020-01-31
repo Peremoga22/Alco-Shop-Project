cd0fbf2e6e3c8d7210a7663841f779f4bf77f696
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2020-01-31 11:38:08 +0200

    add contracts two interfacies

---
+++ scr/WebApplication1/Data/Interfacies/IDrinkRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data.Interfacies
+{
+    public interface IDrinkRepository
+    {
+        IEnumerable<Drink> Drinks { get; }
+        IEnumerable<Drink> PreferredDrinks { get; }
+        Drink GetDrinkById(int drinkId);
+    }
+}
