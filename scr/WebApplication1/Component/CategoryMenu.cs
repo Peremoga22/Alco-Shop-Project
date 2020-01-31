cf37500746503e9f16027b63db4248cb5aa63462
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2020-01-31 11:58:56 +0200

    create Category Menu

---
+++ scr/WebApplication1/Component/CategoryMenu.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data.Interfacies;
+
+namespace WebApplication1.Component
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository categoryRepository;
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = categoryRepository.Categories.OrderBy(p => p.CategoryName);
+            return View(categories);
+        }
+    }
+}
