4abdf338484a8d828fb8ee69497d15d4f42310c2
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2020-02-03 16:28:44 +0200

    Create Repositories
    Drink and Category;

---
+++ scr/WebApplication1/Data/Repositories/DrinkRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Data.Interfacies;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data.Repositories
+{
+    public class DrinkRepository : IDrinkRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public DrinkRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public IEnumerable<Drink> Drinks => _appDbContext.Drinks.Include(c => c.Category);
+
+        public IEnumerable<Drink> PreferredDrinks => _appDbContext.Drinks.Where(p => p.IsPreferredDrink).Include(c => c.Category);
+
+        public Drink GetDrinkById(int drinkId) => _appDbContext.Drinks.FirstOrDefault(p => p.CategoryId == drinkId);
+       
+    }
+}
